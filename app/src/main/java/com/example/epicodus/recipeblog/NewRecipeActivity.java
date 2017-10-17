998bd09da3b194e90bf47cc64015541bca56f648
Haneen Abu-Khater <abukhater.haneen@gmail.com>
2017-10-17 15:46:57 -0700

    add back error and input fields to new recipe page

--- app/src/main/java/com/example/epicodus/recipeblog/NewRecipeActivity.java
+++ app/src/main/java/com/example/epicodus/recipeblog/NewRecipeActivity.java
@@ -2,12 +2,26 @@ package com.example.epicodus.recipeblog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class NewRecipeActivity extends AppCompatActivity {
+
+    @Bind(R.id.editRecipeTitle) EditText mEditRecipeTitle;
+    @Bind(R.id.editIngedient1) EditText mEditIngredient1;
+    @Bind(R.id.editIngedient2) EditText mEditIngredient2;
+    @Bind(R.id.editIngedient3) EditText mEditIngredient3;
+    @Bind(R.id.editDirections) EditText mEditDirections;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_recipe);
+
+        ButterKnife.bind(this);
+
     }
 }
