35ba63033a64170deca933c8217c8895cad7a598
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-10-20 14:09:10 -0700

    add json parsing to service

--- app/src/main/java/com/example/epicodus/recipeblog/YummlyService.java
+++ app/src/main/java/com/example/epicodus/recipeblog/YummlyService.java
@@ -1,10 +1,20 @@
 package com.example.epicodus.recipeblog;
 
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Created by Guest on 10/20/17.
@@ -28,5 +38,35 @@ public class YummlyService {
 
         Call call = client.newCall(request);
         call.enqueue(callback);
+    }
+    public ArrayList<Recipe> processResults(Response response){
+        ArrayList<Recipe> recipes = new ArrayList<>();
+        try{
+            String jsonData = response.body().string();
+
+            JSONObject yummlyJSON = new JSONObject(jsonData);
+            JSONArray resultsJSON = yummlyJSON.getJSONArray("results");
+
+            for (int i=0; i < resultsJSON.length(); i++){
+                JSONObject recipeJSON = resultsJSON.getJSONObject(i);
+
+                String recipeName = recipeJSON.getString("recipeName");
+                int totalTime = recipeJSON.getInt("totalTimeInSeconds");
+                String ingredientsString = recipeJSON.getString("ingredients");
+                int rating = recipeJSON.getInt("rating");
+
+                String [] ingredients = ingredientsString.split(",");
+
+                Recipe recipe = new Recipe(recipeName, totalTime, ingredients, rating);
+                recipes.add(recipe);
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }catch (JSONException e){
+            e.printStackTrace();
+            Log.d("INSIDE JSON ", "test" +e);
+        }
+        return recipes;
     }
 }
