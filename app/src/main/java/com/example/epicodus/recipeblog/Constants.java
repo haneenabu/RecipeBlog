2b45db979f2293ee456569d46814d1dd92acec6b
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-10-20 10:27:11 -0700

    add ApiKey and ApiId

---
+++ app/src/main/java/com/example/epicodus/recipeblog/Constants.java
@@ -0,0 +1,12 @@
+package com.example.epicodus.recipeblog;
+
+/**
+ * Created by Guest on 10/20/17.
+ */
+
+public class Constants {
+    public static final String API_KEY = BuildConfig.API_KEY;
+    public static final String API_ID = BuildConfig.API_ID;
+
+
+}
