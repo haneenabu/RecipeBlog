b70ba7f73d366b57fdf17bbeaf61d81109ab544e
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-10-20 10:44:44 -0700

    add yummlyservice

--- app/src/main/java/com/example/epicodus/recipeblog/Constants.java
+++ app/src/main/java/com/example/epicodus/recipeblog/Constants.java
@@ -7,6 +7,7 @@ package com.example.epicodus.recipeblog;
 public class Constants {
     public static final String API_KEY = BuildConfig.API_KEY;
     public static final String API_ID = BuildConfig.API_ID;
-
+    public static final String YUMMLY_BASE_URL = "http://api.yummly.com/v1/api/recipes";
+    public static final String YUMMLY_FOOD_QUERY_PARAMETER = "food";
 
 }
---
+++ app/src/main/java/com/example/epicodus/recipeblog/YummlyService.java
@@ -0,0 +1,13 @@
+package com.example.epicodus.recipeblog;
+
+import okhttp3.Callback;
+
+/**
+ * Created by Guest on 10/20/17.
+ */
+
+public class YummlyService {
+    public static void findRecipes(String food, Callback callback){
+
+    }
+}
