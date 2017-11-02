2bc3245b2714fecfc394bd4f0fb2faab715f0fd3
Haneen Abu-Khater <abukhater.haneen@gmail.com>
2017-11-02 12:15:19 -0700

    push flex uis fragment

---
+++ app/src/main/java/com/example/epicodus/recipeblog/ui/RecipeListFragment.java
@@ -0,0 +1,99 @@
+package com.example.epicodus.recipeblog.ui;
+
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.preference.PreferenceManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.epicodus.recipeblog.Constants;
+import com.example.epicodus.recipeblog.R;
+import com.example.epicodus.recipeblog.adapters.RecipeListAdapter;
+import com.example.epicodus.recipeblog.models.Recipe;
+import com.example.epicodus.recipeblog.services.YummlyService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+
+public class RecipeListFragment extends Fragment {
+
+    @Bind(R.id.recylclerViewer) RecyclerView mRecyclerView;
+
+    private RecipeListAdapter mAdapter;
+    public ArrayList<Recipe> mRecipes = new ArrayList<>();
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private String mRecentFood;
+
+
+    public RecipeListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstances){
+        super.onCreate(savedInstances);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mEditor = mSharedPreferences.edit();
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_recipe_list, container, false);
+        ButterKnife.bind(this, view);
+
+        mRecentFood = mSharedPreferences.getString(Constants.PREFERENCES_FOOD_KEY, null);
+        if (mRecentFood != null){
+            getRecipes(mRecentFood);
+        }
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+    public void getRecipes(String food){
+        final YummlyService yummlyService = new YummlyService();
+
+        yummlyService.findRecipes(food, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                mRecipes = yummlyService.processResults(response);
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAdapter = new RecipeListAdapter(getActivity(), mRecipes);
+
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
+                    }
+                });
+            }
+        });
+
+    }
+
+}
